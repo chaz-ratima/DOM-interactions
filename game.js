ec9c084e07532d0069b015954d582bb1bf316072
chaz-ratima <65992175+chaz-ratima@users.noreply.github.com>
2020-07-10 11:24:26 +1200

    Add change to game.js

--- game.js
+++ game.js
@@ -12,6 +12,8 @@ function bindEventListeners (dots) {
     // BIND YOUR EVENT LISTENERS HERE
     // The first one is provided for you
     dots[i].addEventListener('contextmenu', makeGreen)
+    dots[i].addEventListener('click', makeBlue)
+    dots[i].addEventListener('dblclick', hide)
   }
 }
 
@@ -23,7 +25,18 @@ function makeGreen (evt) {
 
 // CREATE FUNCTION makeBlue HERE
 
+
+function makeBlue (evt) {
+  evt.target.classList.toggle('blue')
+  updateCounts()
+}
 // CREATE FUNCTION hide HERE
+
+function hide (evt) {
+  evt.target.classList.toggle('invisible')
+  updateCounts()
+}
+
 
 function updateCounts () {
   var totals = {
@@ -33,6 +46,15 @@ function updateCounts () {
   }
   
   // WRITE CODE HERE TO COUNT BLUE, GREEN, AND INVISIBLE DOTS
+
+  var blueDots = document.getElementsByClassName('blue').length
+  totals.blue = blueDots
+
+  var greenDots = document.getElementsByClassName('green').length
+  totals.green = greenDots
+
+  var invisibleDots = document.getElementsByClassName('invisible').length
+  totals.invisible = invisibleDots
 
   // Once you've done the counting, this function will update the display
   displayTotals(totals)
